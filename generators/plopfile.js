76324c8cc9c124d8d519ca8eec76ef0af6f9e2f0
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-05-02 17:35:38 -0300

    chore: add plop for generate files automatic

---
+++ generators/plopfile.js
@@ -0,0 +1,29 @@
+export default function(plop) {
+  plop.setGenerator('component', {
+    description: 'Create logic component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'component name please'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/index.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/test.spec.tsx',
+        templateFile: 'templates/test.spec.tsx.hbs'
+      }
+    ],
+  })
+}
