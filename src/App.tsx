60adc49374d0b2b8163dfb9a1f952e6c830e8b58
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-05-02 19:16:14 -0300

    refactor: change structure for show components

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="bg-gray-200 h-screen w-full">
-      <h1 className="text-9xl font-bold underline text-red-500">Vite + React</h1>
-    </div>
-  )
-}
-
-export default App
