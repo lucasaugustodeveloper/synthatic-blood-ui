cad70ae0c6691dd964eb9da2b31b1868a9b02e63
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-05-02 16:21:21 -0300

    chore: add vite dts and config for build project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true
+    })
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'synthatic-blood-ui',
+      formats: ['es', 'umd'],
+      fileName: (format) => `synthatic-blood-ui.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'tailwindcss'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          tailwindcss: 'tailwindcss'
+        }
+      }
+    }
+  }
 })
