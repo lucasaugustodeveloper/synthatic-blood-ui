7a5ed82143a9e738d2da2036680de9e1e89ff57b
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-05-02 18:44:40 -0300

    chore: add storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,17 @@
-export default {
-  stories: ["../src/**/*/stories.mdx", "../src/**/*/stories.@{ts|tsx}"],
+import type { StorybookConfig } from "@storybook/react-vite";
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  framework: "@storybook/react",
-  core: {
-    builder: "storybook-builder-vite"
-  }
-}
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
